e51e88bde97e551386f8a6df0e3b842e7cf4a306
Emeka Allison <emekaallison4@gmail.com>
2026-06-29 21:47:05 +0100

    Initialized Fortiva

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="title">Fortiva</h1>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+.title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: #045128;
+}
+
+[data-theme="dark"] .title {
+  color: #00ff00;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Swap from NGN to USD</h1>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 2rem;
+    font-weight: semi-bold;
+    margin-bottom: 1rem;
+    text-align: center;
+    color: var(--color);
+}
+</style>
